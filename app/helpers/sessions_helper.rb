d00de56dad5541f80f87c024b7634228998c11a3
arctech606 <arctech606@gmail.com>
2016-04-04 13:38:55 +0000

    Till section 8.4.1

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,14 @@ module SessionsHelper
   def log_in(faculty)
     session[:faculty_id] = faculty.id
   end
+  def current_faculty
+    @current_faculty ||= Faculty.find_by(id: session[:faculty_id])
+  end
+  def logged_in?
+    !current_faculty.nil?
+  end
+  def log_out
+    session.delete(:faculty_id)
+    @current_faculty = nil
+  end
 end
