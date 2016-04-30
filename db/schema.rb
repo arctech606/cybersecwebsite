e6c84a1ab463c8ca0a1147a78b536f8d616080d2
Sujoy Saha <sujoysaha7@gmail.com>
2016-04-30 04:27:36 +0000

    added uin to faculty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430031357) do
+ActiveRecord::Schema.define(version: 20160430040718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160430031357) do
     t.datetime "updated_at",           null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.integer  "uin"
   end
 
   create_table "faculties_publications", force: :cascade do |t|
