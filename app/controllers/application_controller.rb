6e400b69ede885d15f6caf1662ba8c5c2cf3209c
swetha3294 <swethasridharan1236@gmail.com>
2016-04-30 03:04:45 +0000

    faculty nav done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include StudentsSessionsHelper
+
+  helper_method :current_user
+
+  private
+  def current_user
+  @current_user ||= Faculty.find(session[:faculty_id]) if session[:faculty_id]
+  rescue ActiveRecord::RecordNotFound
+    session[:faculty_id] = nil
+  end
+  
+  def current_student_user
+  @current_student_user ||= Student.find(session[:student_id]) if session[:student_id]
+  rescue ActiveRecord::RecordNotFound
+    session[:student_id] = nil
+  end
 end
