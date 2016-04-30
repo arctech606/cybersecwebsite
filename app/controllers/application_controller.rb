12cbc6da1660e646454aeafe9db549ba871ac624
swetha3294 <swethasridharan1236@gmail.com>
2016-04-30 04:18:38 +0000

    login done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,5 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-  private
-  def current_user
-  @current_user ||= Faculty.find(session[:faculty_id]) if session[:faculty_id]
-  rescue ActiveRecord::RecordNotFound
-    session[:faculty_id] = nil
-  end
   
-  def current_student_user
-  @current_student_user ||= Student.find(session[:student_id]) if session[:student_id]
-  rescue ActiveRecord::RecordNotFound
-    session[:student_id] = nil
-  end
 end
